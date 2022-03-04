d88048f0e4462ab1ee127f0990ae5b416eae3df9
egersack <egersack@mit.edu>
2022-03-04 10:28:17 -0500

    commit

--- book_industry.sql
+++ book_industry.sql
@@ -5,6 +5,42 @@ USE `bookbusiness`;
 SET NAMES UTF8MB4;
 SET character_set_client = UTF8MB4;
 
+CREATE TABLE `customer` (
+    `customer_firstname`     varchar(20) NOT NULL,
+    `customer_lastname`      varchar(20) NOT NULL,
+    `customer_UID`           int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`customer_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `order` (
+    `customer_UID`          int NOT NULL,
+    `order_date`            varchar(20) NOT NULL,
+    `order_UID`             int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`order_UID`),
+    FOREIGN KEY (`customer_UID`) REFERENCES `customer`(`customer_UID`),
+    INDEX (`customer_UID`, `order_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `author` (
+    `author_firstname`     varchar(20) NOT NULL,
+    `author_lastname`      varchar(20) NOT NULL,
+    `author_UID`           int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`author_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `publisher` (
+    `publisher_name`        varchar(20) NOT NULL,
+    `publisher_UID`         int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`publisher_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `editor` (
+    `editor_firstname`     varchar(20) NOT NULL,
+    `editor_lastname`      varchar(20) NOT NULL,
+    `editor_UID`           int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`editor_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `book` (
     `title`         varchar(20) NOT NULL,
     `genre`         varchar(20) NOT NULL,
@@ -15,41 +51,30 @@ CREATE TABLE `book` (
     PRIMARY KEY (`ISBN`),
     FOREIGN KEY (`author_UID`) REFERENCES `author`(`author_UID`),
     FOREIGN KEY (`editor_UID`) REFERENCES `editor`(`editor_UID`),
+    INDEX (`author_UID`, `editor_UID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `author` (
-    `author_firstname`     varchar(20) NOT NULL,
-    `author_lastname`      varchar(20) NOT NULL,
-    `author_UID`           int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`author_UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `editor` (
-    `editor_firstname`     varchar(20) NOT NULL,
-    `editor_lastname`      varchar(20) NOT NULL,
-    `editor_UID`           int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`editor_UID`),
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `publisher` (
-    `publisher_name`        varchar(20) NOT NULL,
-    `publisher_UID`         int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`publisher_UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `customer` (
-    `customer_firstname`     varchar(20) NOT NULL,
-    `customer_lastname`      varchar(20) NOT NULL,
-    `customer_UID`           int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`customer_UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `order` (
-    `customer_UID`          int NOT NULL,
-    `ISBN`                  int NOT NULL,
-    `order_date`            varchar(20) NOT NULL,
-    `order_UID`             int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`order_UID`),
-    FOREIGN KEY (`customer_UID`) REFERENCES `customer`(`customer_UID`),
+CREATE TABLE `junction_author_book` (
+    `author_UID` int NOT NULL,
+    `ISBN` int NOT NULL,
+    PRIMARY KEY (`author_UID`, `ISBN`),
     FOREIGN KEY (`ISBN`) REFERENCES `book`(`ISBN`),
+    FOREIGN KEY (`author_UID`) REFERENCES `author`(`author_UID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `junction_order_book` (
+    `order_UID` int NOT NULL,
+    `ISBN` int NOT NULL,
+    PRIMARY KEY (`order_UID`, `ISBN`),
+    FOREIGN KEY (`order_UID`) REFERENCES `order`(`order_UID`),
+    FOREIGN KEY (`ISBN`) REFERENCES `book`(`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `junction_editor_book` (
+    `editor_UID` int NOT NULL,
+    `ISBN` int NOT NULL,
+    PRIMARY KEY (`editor_UID`, `ISBN`),
+    FOREIGN KEY (`editor_UID`) REFERENCES `editor`(`editor_UID`),
+    FOREIGN KEY (`ISBN`) REFERENCES `book`(`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+
---
+++ sample_data.sql
@@ -0,0 +1,69 @@
+USE `bookbusiness`;
+
+INSERT INTO `book` VALUES(1,1,'book1', 'fantasy');
+INSERT INTO `book` VALUES(2,1,'book2', 'fantasy');
+INSERT INTO `book` VALUES(3,2,'book3', 'sci-fi');
+INSERT INTO `book` VALUES(4,2,'book4', 'horror');
+INSERT INTO `book` VALUES(5,2,'book5', 'fantasy');
+INSERT INTO `book` VALUES(6,3,'book6', 'sci-fi');
+INSERT INTO `book` VALUES(7,4,'book7', 'realism');
+INSERT INTO `book` VALUES(8,5,'book8', 'non-fiction');
+INSERT INTO `book` VALUES(9,5,'book9', 'non-fiction');
+INSERT INTO `book` VALUES(10,6,'book10', 'fantasy');
+
+INSERT INTO `author` VALUES(1,'Ciliren', 'Eilmoira');
+INSERT INTO `author` VALUES(2,'Llorva', 'Ulawenys');
+INSERT INTO `author` VALUES(3,'Elred', 'Trawynn');
+INSERT INTO `author` VALUES(4,'Volodar', 'Zylyarus');
+INSERT INTO `author` VALUES(5,'Anarzee', 'Elaran');
+INSERT INTO `author` VALUES(6,'Ildiyntra', 'Jorieth');
+INSERT INTO `author` VALUES(7,'Gormar', 'Inagella');
+INSERT INTO `author` VALUES(8,'Aelua', 'Ulawenys');
+INSERT INTO `author` VALUES(9,'Anallee', 'Dorbella');
+
+INSERT INTO `editor` VALUES(1,'Inchel', 'Keazeiros');
+INSERT INTO `editor` VALUES(2,'Emmyth', 'Gilstina');
+INSERT INTO `editor` VALUES(3,'Aerith', 'Zinharice');
+INSERT INTO `editor` VALUES(4,'Tannatar', 'Balkian');
+INSERT INTO `editor` VALUES(5,'Yaereene', 'Elbalar');
+INSERT INTO `editor` VALUES(6,'Shalheira', 'Iarstina');
+INSERT INTO `editor` VALUES(7,'Inchel', 'Wysazorwyn');
+
+INSERT INTO `publisher` VALUES(1,'Athtar', 'Wysahice');
+INSERT INTO `publisher` VALUES(2,'Ena', 'Miadithasce');
+INSERT INTO `publisher` VALUES(3,'Drannor', 'Davaris');
+INSERT INTO `publisher` VALUES(4,'Soliana', 'Genwynn');
+INSERT INTO `publisher` VALUES(5,'Shalheira', 'Keygella');
+INSERT INTO `publisher` VALUES(6,'Makaela', 'Keasatra');
+
+INSERT INTO `customer` VALUES(1, 'Arlen', 'Loraxisys');
+INSERT INTO `customer` VALUES(2, 'Shenarah', 'Thenelis');
+INSERT INTO `customer` VALUES(3, 'Nithroel', 'Jonelis');
+INSERT INTO `customer` VALUES(4, 'Isilynor', 'Liazorwyn');
+INSERT INTO `customer` VALUES(5, 'Malon', 'Olakas');
+INSERT INTO `customer` VALUES(6, 'Jorldyn', 'Liabella');
+INSERT INTO `customer` VALUES(7, 'Arathorn', 'Genwynn');
+INSERT INTO `customer` VALUES(8, 'Haldir', 'Rohice');
+INSERT INTO `customer` VALUES(9, 'Agandaur', 'Ianphyra');
+INSERT INTO `customer` VALUES(10, 'Tarathiel', 'Shafaren');
+INSERT INTO `customer` VALUES(11, 'Almithara', 'Miadi');
+INSERT INTO `customer` VALUES(12, 'Sudryal', 'Heiwynn');
+INSERT INTO `customer` VALUES(13, 'Garynnon', 'Ertris');
+INSERT INTO `customer` VALUES(14, 'Arlen', 'Daceran');
+INSERT INTO `customer` VALUES(15, 'Egalmoth', 'Arch');
+
+INSERT INTO `order` VALUES(1,1);
+INSERT INTO `order` VALUES(2,2);
+INSERT INTO `order` VALUES(3,3);
+INSERT INTO `order` VALUES(4,4);
+INSERT INTO `order` VALUES(5,5);
+INSERT INTO `order` VALUES(6,6);
+INSERT INTO `order` VALUES(7,7);
+INSERT INTO `order` VALUES(8,8);
+INSERT INTO `order` VALUES(9,9);
+INSERT INTO `order` VALUES(10,10);
+INSERT INTO `order` VALUES(11,11);
+INSERT INTO `order` VALUES(12,12);
+INSERT INTO `order` VALUES(13,13);
+INSERT INTO `order` VALUES(14,14);
+INSERT INTO `order` VALUES(15,15);
